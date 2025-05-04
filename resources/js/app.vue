08c38eee0d2a0b09293ce589798aac649768b2df
Jonatas <jonatasmc04@gmail.com>
2025-05-04 16:47:38 -0300

    first commit: configuração inicial do projeto com laravel e banco de dados MySQL

---
+++ resources/js/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>PicPay Simplificado</h1>
+    <p>Teste de Transferência</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PicPay Simplificado</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
